eaafaf66a136bdac47c4fa1cd66a5d4c5153887d
ibraxim <ibraximaliev45@mail.ru>
2023-09-27 17:14:49 +0300

    new

--- js/carousel.js
+++ js/carousel.js
@@ -3,9 +3,17 @@ $(document).ready(function () {
 	const owl = $('.slider');
 	// Запускаем карусель
 	owl.owlCarousel({
-        slideBy:1,
+		items: 1,
+        slideBy: 1,	
         center: true,
-        loop: true
+        loop: true,
+		responsive: {
+			1199: {
+				items: 2.1
+			}
+		},
+		autoWidth: true
+		
 	});
 
 	// Находим кастомные кнопки Вперед / Назад
